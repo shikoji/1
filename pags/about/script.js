6e9fad37c627850a7c55f188c054734b6a83ab8a
shikoji <robsongomes@acad.ifma.edu.br>
2025-03-30 00:11:18 -0300

    Add files via upload

---
+++ pags/about/script.js
@@ -0,0 +1,11 @@
+export default () => {
+    const container = document.createElement("div");
+
+    const template = `
+        <h2>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</h2>
+    ´`;
+
+    container.innerHTML = template;
+
+    return container;
+}
